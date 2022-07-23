57d0149328a619ba9db89cdf83ae9e417d67a690
Fred Garcia <Fred@Garcia.com>
2022-07-23 16:59:27 -0700

    first commit

---
+++ pages/[slug].js
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import {marked} from 'marked'
+
+
+
+export default function PostPage({postData, content}) {
+  return (
+    <Layout>
+          <Head>
+        <title>Fred's Blog</title>
+      </Head>
+       {/* Main */}
+      <div id="main" className="alt">
+        {/* One */}
+        <section id="one">
+          <div className="inner">
+            <header className="major">
+              <h1>{postData.title}</h1>
+            </header>
+            <span className="image main"><img src={`images/${postData.featured_image}`} alt="" /></span>
+            <div dangerouslySetInnerHTML={{__html: content}}/>
+          </div>
+        </section>
+      </div>
+      </Layout>
+  )
+}
+
+export const getStaticPaths = async() =>  {
+  const files = fs.readdirSync('posts');
+  console.log("Files: ", files);
+  const paths = files.map(filename => ({
+    params: {
+      slug: filename.replace('.md', '')
+    }
+  }));
+  console.log('paths: ', paths);
+  return {
+    paths: paths,
+    fallback: false
+  }
+};
+export const getStaticProps = async ({params: {slug}}) => {
+  const post = fs.readFileSync(path.join('posts', `${slug}.md`)).toString();
+  const postData = matter(post); // gets post data
+  const content = marked(postData.content); //convets into html
+  console.log(postData.data)
+  return {
+    props: {
+      slug,
+      content,
+      postData:postData.data
+    }
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,95 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import moment from 'moment'
+import Link from 'next/link'
+
+
+export default function Home({posts}) {
+  return (
+    <Layout>
+          <Head>
+            <title>Fred's Blog</title>
+          </Head>
+        {/* Banner */}
+        <section id="banner" className="major">
+          <div className="inner">
+            <header className="major">
+              <h1>Hi, my name is Forty</h1>
+            </header>
+            <div className="content">
+              <p>A responsive site template designed by HTML5 UP<br />
+                and released under the Creative Commons.</p>
+              <ul className="actions">
+                <li><a href="#one" className="button next scrolly">Get Started</a></li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        {/* Main */}
+        <div id="main">
+
+          {/* One */}
+          <section id="one" className="tiles">
+            {/* loop over post */}
+
+            {posts.map((post) => (
+                    <article>
+                    <span className="image">
+                      <img src={`/images/${post.featured_image}`} alt="" />
+                    </span>
+                    <header className="major">
+                      <h3><Link href={`${post.slug}`}className="link">{post.title}</Link></h3>
+                      {/* <p>Feugiat amet tempus</p> */}
+                    </header>
+                  </article>
+            ))}
+      
+          </section>
+          {/* Two */}
+          <section id="two">
+            <div className="inner">
+              <header className="major">
+                <h2>Massa libero</h2>
+              </header>
+              <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+              <ul className="actions">
+                <li><a href="landing.html" className="button next">Get Started</a></li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </Layout>
+  )
+}
+
+export const getStaticProps = async () => {
+  const sortPosts = () => {
+    const allPosts = fs.readdirSync('posts').map((filename) => {
+      const file = fs.readFileSync(path.join("posts", filename)).toString();
+      // console.log(file);
+      const postData = matter(file);
+      // console.log(postData);
+      return {
+        content: postData.content,
+        title: postData.data.title,
+        featured_image: postData.data.featured_image,
+        date: postData.data.date,
+        slug: postData.data.slug
+      };
+    });
+    // console.log(allPosts);
+    return allPosts.sort((a,b) => new moment(b.date, 'YYYY-MM-DD HH:mm:ss') - new moment(a.date, 'YYYY-MM-DD HH:mm:ss'));
+    
+  }
+  // console.log(fs.readdirSync('posts'));
+ console.log(sortPosts());
+  return {
+    props: {
+      posts: sortPosts(),
+    }
+  }
+}
+
